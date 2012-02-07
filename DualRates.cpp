bf86c1bbc68b7997fa28f849e0c2a0b495e2e72e
dvdouden <dvdouden@b3f6647d-59c9-437c-967c-7e6ecf1a32c7>
2012-02-07 16:08:42 +0000

    ADD: #1 First implementation of Expo ADD: #1 First implementation of Dual Rates CHG: Keywords added

---
+++ DualRates.cpp
@@ -0,0 +1,30 @@
+#include <DualRates.h>
+
+
+namespace tx
+{
+
+// Public functions
+
+DualRates::DualRates()
+{
+	
+}
+
+
+int16_t DualRates::apply(int16_t p_value, uint8_t p_rate) const
+{
+	// there's a risk in overflows here, since 256 * 140 > 32K
+	// so we do this unsigned..
+	uint8_t neg = p_value < 0;
+	uint16_t val = static_cast<uint16_t>(neg ? (-p_value) : p_value);
+	val = (val * p_rate) / 100;
+	return neg ? -static_cast<int16_t>(val) : static_cast<int16_t>(val);
+}
+
+// Define global instance
+DualRates g_DualRates = DualRates();
+
+
+// namespace end
+}
---
+++ Expo.h
@@ -0,0 +1,44 @@
+#ifndef INC_TX_EXPO_H
+#define INC_TX_EXPO_H
+
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** Expo.h
+** Exponential functionality
+**
+** Author: Daniel van den Ouden
+** Project: ArduinoTXLib
+** Website: http://sourceforge.net/p/arduinotxlib/
+** -------------------------------------------------------------------------*/
+
+#include <inttypes.h>
+
+
+namespace tx
+{
+
+/*! \brief Class to encapsulate Exponential functionality.*/
+class Expo
+{
+public:
+	/*! \brief Constructs an Expo object
+	*/
+	Expo();
+	
+	/*! \brief Applies expo.
+	    \p_value Source value to apply expo to, range [-256 - 256].
+		\p_expo Amount of expo to apply, range [-100 - 100].
+		\return expo applies p_value.*/
+	int16_t apply(int16_t p_value, int8_t p_expo) const;
+	
+private:
+};
+
+extern Expo g_Expo; //!< Global instance
+
+} // namespace end
+
+#endif
