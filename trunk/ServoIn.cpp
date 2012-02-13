12fba6c82304b2c1e8c0e9ed1c4a9ba536fa4486
dvdouden <dvdouden@b3f6647d-59c9-437c-967c-7e6ecf1a32c7>
2012-02-13 07:35:46 +0000

    ADD: #16 Sero input placeholder ADD: #17 Servo output placeholder

---
+++ trunk/ServoIn.cpp
@@ -0,0 +1,33 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** ServoIn.cpp
+** Servo Signal Input functionality
+**
+** Author: Daniel van den Ouden
+** Project: ArduinoRCLib
+** Website: http://sourceforge.net/p/arduinorclib/
+** -------------------------------------------------------------------------*/
+
+#include <ServoIn.h>
+
+
+namespace rc
+{
+
+// Public functions
+
+ServoIn::ServoIn()
+{
+	
+}
+
+
+// Private functions
+
+
+
+// namespace end
+}
