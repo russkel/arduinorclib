40ff47e2d24e36953fa0086df599ee3ec85f4d4f
dvdouden <dvdouden@b3f6647d-59c9-437c-967c-7e6ecf1a32c7>
2012-02-14 11:37:27 +0000

    ADD: #16 Initial version of ServoIn

--- ServoIn.cpp
+++ ServoIn.cpp
@@ -11,7 +11,15 @@
 ** Website: http://sourceforge.net/p/arduinorclib/
 ** -------------------------------------------------------------------------*/
 
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include <Arduino.h>
+#else
+	#include <avr/interrupt.h>
+	#include <wiring.h>
+#endif
+
 #include <ServoIn.h>
+#include <util.h>
 
 
 namespace rc
@@ -20,13 +28,101 @@ namespace rc
 // Public functions
 
 ServoIn::ServoIn()
+:
+m_state(State_Startup),
+m_ticks(1520),
+m_lastTime(0)
 {
-	
+	// check if Timer 1 is running or not
+	if ((TCCR1B & ((1 << CS12) | (1 << CS11) | (1 << CS10))) == 0)
+	{
+		// start Timer 1
+		TCCR1B |= (1 << CS11);
+	}
 }
 
 
-// Private functions
+bool ServoIn::isStable() const
+{
+	return m_state == State_Stable;
+}
 
+
+void ServoIn::pinChanged(bool p_high)
+{
+	// first things first, get Timer 1 count
+	uint8_t oldSREG = SREG;
+	cli();
+	uint16_t cnt = TCNT1;
+	SREG = oldSREG;
+	
+	switch (m_state)
+	{
+	default:
+	case State_Startup:
+		{
+			if (p_high)
+			{
+				m_state = State_Listening;
+			}
+		}
+		break;
+	
+	case State_Listening:
+		{
+			if (p_low)
+			{
+				if (cnt - m_lastTime >= 6000)
+				{
+					m_state = State_Confused;
+				}
+				else
+				{
+					m_ticks = cnt - m_lastTime;
+					m_state = State_Stable;
+				}
+			}
+		}
+		break;
+	
+	case State_Stable:
+		{
+			if (p_high)
+			{
+				if (cnt - m_lastTime > 30000)
+				{
+					m_state = State_Confused;
+				}
+			}
+			else
+			{
+				if (cnt - m_lastTime >= 6000)
+				{
+					m_state = State_Confused;
+				}
+				else
+				{
+					m_ticks = cnt - m_lastTime;
+				}
+			}
+		}
+		break;
+	}
+	m_lastTime = cnt;
+}
+
+
+int16_t ServoIn::getValue(bool p_useMicroseconds)
+{
+	if (p_useMicroseconds)
+	{
+		return m_ticks >> 1;
+	}
+	else
+	{
+		return microsToNormalized(m_ticks >> 1);
+	}
+}
 
 
 // namespace end
